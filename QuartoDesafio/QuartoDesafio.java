5c737fe73247d252112d3517d5104e85c96a46a9
Victoria Trois <vickytrois@outlook.com>
2023-08-06 13:24:57 -0300

    Implementação desafio 04 - versão 01
    Esta mudança:
    * Implementa os requisitos descritos no desafio 04.

--- QuartoDesafio/QuartoDesafio.java
+++ QuartoDesafio/QuartoDesafio.java
@@ -1,5 +1,7 @@
 package QuartoDesafio;
 
+import java.util.Scanner;
+
 public class QuartoDesafio {
     /*
      * A sua impressora foi infectada por um vírus e está imprimindo de forma incorreta. Depois
@@ -36,6 +38,66 @@ public class QuartoDesafio {
      * VOD OWT SNEH HCNERF EGDIRTRAP A DNA SE
      */
     public static void main(String[] args) {
+        Scanner entrada;
+        int numeroDeLinhas;
+        String[] linhasCifradas;
+        int numeroDeLinhasFornecidas;
 
+        entrada = new Scanner(System.in);
+        linhasCifradas = null;
+        numeroDeLinhas = 0;
+        numeroDeLinhasFornecidas = 0;
+
+        System.out.print("Digite o número de linhas que deseja decifrar: ");
+        numeroDeLinhas = entrada.nextInt();
+        entrada.nextLine();
+
+        linhasCifradas = new String[numeroDeLinhas];
+
+        while (numeroDeLinhasFornecidas < numeroDeLinhas) {
+            System.out.println("Digite a " + (numeroDeLinhasFornecidas+1) + "ª linha:");
+            String linhaCifrada = entrada.nextLine();
+            if (linhaCifrada.length() % 2 != 0) {
+                System.out.println("Entrada inválida. Esta solução opera somente com linhas de número de caracteres pares.");
+            } else {
+                linhasCifradas[numeroDeLinhasFornecidas] = linhaCifrada;
+                numeroDeLinhasFornecidas++;
+            }
+        }
+
+        for (int i = 0; i < numeroDeLinhas; i++) {
+            String linhaDecifrada = decifraLinha(linhasCifradas[i]);
+            System.out.println(linhaDecifrada);
+        }
+    }
+
+    public static String decifraLinha(String linhaCifrada) {
+        int tamanhoDaLinha;
+        int metadeDoTamanho;
+        String metadeEsquerda;
+        String metadeDireita;
+        StringBuilder linhaDecifrada;
+
+        tamanhoDaLinha = linhaCifrada.length();
+        metadeDoTamanho = tamanhoDaLinha / 2;
+        metadeEsquerda = linhaCifrada.substring(0, metadeDoTamanho);
+        metadeDireita = linhaCifrada.substring(metadeDoTamanho);
+
+        linhaDecifrada= new StringBuilder();
+
+        for (int i = metadeDoTamanho - 1; i >= 0; i--) {
+            linhaDecifrada.append(metadeEsquerda.charAt(i));
+        }
+
+        for (int i = metadeDoTamanho - 1; i >= 0; i--) {
+            linhaDecifrada.append(metadeDireita.charAt(i));
+        }
+
+        return linhaDecifrada.toString();
     }
 }
+
+
+
+
+
